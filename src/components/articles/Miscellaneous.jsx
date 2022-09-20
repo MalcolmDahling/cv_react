bde286af283e24c7e61b1040c3529f26cef45a99
MalcolmDahling <mac.dahling@gmail.com>
2022-09-20 15:08:01 +0200

    Update Miscellaneous.jsx

--- src/components/articles/Miscellaneous.jsx
+++ src/components/articles/Miscellaneous.jsx
@@ -7,6 +7,8 @@ export function Miscellaneous(){
             <h3>Övrigt</h3> 
             <p>
                 B-körkort.
+                <br/>
+                På fritiden så gillar jag att styrketräna, cykla, och att spela biljard, schack och datorspel.
             </p>
         </article>
     );
